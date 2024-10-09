c0d367175c8fbc88edfb4f453ef3a7e49a5f345d
borriskiz <borisnechaev2004@yandex.ru>
2024-10-09 19:54:33 +0500

    made models

--- django2/settings.py
+++ django2/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,6 +26,8 @@ SECRET_KEY = 'django-insecure-gnw(nl6agh8x^hebphi=xa*yimeon4pt%x0-)8ujxyy9#$9myc
 DEBUG = True
 
 ALLOWED_HOSTS = []
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Application definition
